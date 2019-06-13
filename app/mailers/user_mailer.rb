55c9e1f4baa7a3f0187f93b85d9f8b4c02a79b10
JoshuaHarris93 <josh@emptychairproductions.com>
2019-06-13 03:10:03 +0000

    add erb user mailer template

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,8 +2,8 @@ class UserMailer < ApplicationMailer
 
     def welcome_email
         @user = params[:user]
-        @url  = 'http://example.com/login'
-        mail(to: @user.email, subject: 'Welcome to My Awesome Site')
-      end
-      
+        @url = '/login'
+        mail(to: @user.email, subject: 'Thank you for your order!')
+    end
+    
 end
