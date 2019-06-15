983145f05222656080e8929ea7e349f6e60ca438
JoshuaHarris93 <josh@emptychairproductions.com>
2019-06-15 21:53:44 +0000

    setup product reviews display

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,10 +2,17 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    @reviews = Review.all
   end
 
   def show
     @product = Product.find params[:id]
+    @review = Review.new
   end
 
 end
+
+
+
+
+    
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  resources :products, only: [:index, :show]
+  resources :products do
+    resources :reviews, only: [:create]
+  end
+  
   resources :categories, only: [:show]
 
   resource :cart, only: [:show] do
@@ -27,6 +30,9 @@ Rails.application.routes.draw do
   get '/logout' => 'sessions#destroy'
 
   get '/cart' => 'cart#show'
+
+  # post '/reviews' => 'reviews#create'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
