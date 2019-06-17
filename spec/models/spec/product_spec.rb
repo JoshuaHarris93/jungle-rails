0388cbfb665122390074498edf2a3e30a12ca78b
JoshuaHarris93 <josh@emptychairproductions.com>
2019-06-17 17:35:47 +0000

    first rspec unit tests working properly

---
+++ spec/models/spec/product_spec.rb
@@ -0,0 +1,62 @@
+require 'rails_helper'
+
+RSpec.describe Product, type: :model do
+
+  context "when name, price, quantity, and category is not provided, " do
+    
+    it 'is not valid' do
+      @category = Category.new
+      @product = Product.new
+      @product.category = @category
+      
+      @product.name = "Josh"
+      @product.price = 25
+      @product.quantity = 3
+
+      expect(@product).to be_valid
+    end
+
+    it 'name is not valid' do
+      @category = Category.new
+      @product = Product.new
+      @product.category = @category
+
+      @product.name = nil
+      @product.price = 25
+      @product.quantity = 3
+      expect(@product).to_not be_valid
+    end
+    
+    it 'price is not valid' do
+      @category = Category.new
+      @product = Product.new
+      @product.category = @category
+      
+      @product.name = "Electric Chair"
+      @product.price = nil
+      @product.quantity = 3
+      expect(@product).to_not be_valid
+    end
+
+    it 'quantity is not valid' do
+      @category = Category.new
+      @product = Product.new
+      @product.category = @category
+
+      @product.name = "Red Bookshelf"
+      @product.price = 2483.75
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+
+    it 'categories are not valid' do
+      @category = Category.new
+      @product = Product.new
+
+      @product.name = "Hotdog Slicer"
+      @product.price = 26
+      @product.category = nil
+      expect(@product).to_not be_valid
+    end 
+  end
+end
